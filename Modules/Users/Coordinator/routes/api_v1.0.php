c85c33fab41252ff4e25fc18e58c208b8ee2f622
ToeOoWaiYan <toeoowaiyan4252@gmail.com>
2025-04-06 12:55:24 +0630

    Get All Coordinator Request Permission Checked

--- Modules/Users/Coordinator/routes/api_v1.0.php
+++ Modules/Users/Coordinator/routes/api_v1.0.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Users\Coordinator\App\Http\Controllers\CoordinatorApiController;
 
-Route::apiResource('coordinators', CoordinatorApiController::class);
+Route::apiResource('coordinators', CoordinatorApiController::class)->middleware('auth');
